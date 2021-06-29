c22ce49c426c6c9c5be4d3927f8df9d837350f52
frank <franktranvantu@gmail.com>
2021-06-29 22:27:51 +0700

    Conditional Statements

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-print(10 > 3)
-print(10 >= 3)
-print(10 < 20)
-print(10 <= 20)
-print(10 == 10)
-print(10 == "10")
-print(10 != "10")
-print("bag" > "apply")
-print("bag" == "BAG")
-print(ord("b"))
-print(ord("B"))
+temperature = 15
+if temperature > 30:
+  print("It's warm")
+  print("Drink water")
+elif temperature > 20:
+  print("It's nice")
+else:
+  print("It's cold")
+print("Done")
