2a81b625c1d4f82b70fd15c72e6f083037fd5ab5
frank <franktranvantu@gmail.com>
2021-06-29 23:59:20 +0700

    For..Else

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# for number in range(3):
-#   print("Attempt", number + 1, "." * (number + 1))
-
-# for number in range(1, 4):
-#   print("Attempt", number, "." * number)
-
-for number in range(1, 10, 2):
-  print("Attempt", number, "." * number)
+successful = True
+for number in range(3):
+  print("Attempt")
+  if successful:
+    print("Successful")
+    break
+else:
+  print("Attempted 3 times and failed")
